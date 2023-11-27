a8c69335dc34649085434f6cc32211df4724bbda
Samir Mahapatra <samirmahapatra16@gmail.com>
2023-11-28 00:27:09 +0530

    mend

--- models/postSchema.js
+++ models/postSchema.js
@@ -4,7 +4,6 @@ const { commentSchema } = require("./commentSchema");
 const postSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   author: { type: String, requried: true },
-  private: { type: Boolean, default: false },
   title: { type: String },
   description: { type: String },
   like: { type: Boolean, default: false },
